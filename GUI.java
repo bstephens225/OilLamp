4349f43683c8a483233b584b1b7972ec09432263
bstephens225 <bstephens@smith.edu>
2023-04-30 23:26:13 -0400

    will take in text and print it +response also dim

--- GUI.java
+++ GUI.java
@@ -1,9 +1,14 @@
 import java.awt.*;
 import javax.swing.*;
+
 import java.awt.event.*;
 
 public class GUI{
-        
+    int brightness=255;
+    Color light=new Color(brightness,brightness,brightness);
+    JTextField textField;
+    JTextArea paragraph;
+    String response;
     public GUI() {
         JFrame frame = new JFrame("LampLight");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -11,41 +16,68 @@ public class GUI{
         frame.setLayout(new BorderLayout());
         
         // set the background image
-        ImageIcon background = new ImageIcon("backgroundlamplighttrial.png");
+        ImageIcon background = new ImageIcon("lamplightbackground.png");
         JLabel labelBackground = new JLabel(background);
         labelBackground.setLayout(null);
         
         
         // add label
-        JLabel text = new JLabel("Hi");
-        text.setForeground(Color.white);
-        text.setBounds(400,550,100,20);        
+        paragraph = new JTextArea("You wake up in a Victorian era mansion. You don’t remember how you got there. It is dark except for a gas lamp which will not run out or turn off.");
+        paragraph.setForeground(light);
+        paragraph.setBackground(Color.black);
+        paragraph.setBounds(350,350,400,100);
+        paragraph.setEditable(false);
+        paragraph.setLineWrap(true);
+        paragraph.setWrapStyleWord(true);
+
+       
+        javax.swing.border.Border tfBorder = BorderFactory.createLineBorder(Color.white);
+        //javax.swing.cursor.Cursor tfCursor = (Color.white);
 
         //add text field
-        JTextField textField = new JTextField(20);
-        textField.setForeground(Color.white);
+        textField = new JTextField(20);
+        textField.setForeground(light);
         textField.setBackground(Color.black);
-        textField.setBounds(400,500,100,20);
-
-        labelBackground.add(text);
+        textField.setBorder(tfBorder);
+        textField.setBounds(350,480,250,20);
+        textField.setCaretColor(light);
+        labelBackground.add(paragraph);
         labelBackground.add(textField);
 
         frame.add(labelBackground);
         frame.setVisible(true);
+
+        textField.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //save text and clear text field
+                String command = textField.getText();
+                textField.setText("");
+                
+                paragraph.setText(command+"\n\n"+response);
+                dimBrightness();
+            }
+        });
+    }
+
+    public String setResponse(String response){
+        this.response=response;
+        return response;
+    }
+    public void dimBrightness(){
+        if(brightness>0){brightness-=5;
+            Color darker=new Color(brightness,brightness,brightness);
+            textField.setCaretColor(darker);
+            paragraph.setForeground(darker);
+            textField.setForeground(darker);
+        }
+        
+    //light(brightness,brightness,brightness);
     }
     
     public static void main(String[] args) {
+        String resp="ohh";
         new GUI();
-    }
-    
-
-    public void actionPerformed(ActionEvent evt) {
-        try {
-            //String host = tf.getText();
-            //l.setText(host);
-        } catch (Exception e) {
-            //l.setText(e.getMessage());
-        }
     }
     
 }
